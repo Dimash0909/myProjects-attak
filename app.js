463ce526f108f2eae9acbb2c142ebc0b0e210f68
Dinmukhammed <tastanbekdimash09@gmail.com>
2024-11-08 02:22:28 +0500

    create login and signup pages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes')
 require('dotenv').config()
 
 const app = express();
@@ -22,5 +23,6 @@ mongoose.connect(process.env.dbURI)
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes)
 
 // write to the terminal: npm run server
