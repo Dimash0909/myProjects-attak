07293a512e3239e4f98cacc0ce692e86929e89ec
Dinmukhammed <tastanbekdimash09@gmail.com>
2024-11-08 21:05:46 +0500

    using mongoose hooks to handle create & save operations

--- models/User.js
+++ models/User.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
 
+const print = (...vals) => {
+    console.log(...vals)
+} 
+
 const userSchema = new mongoose.Schema({  // defines the structure
     email: {
         type: String,
@@ -14,6 +18,18 @@ const userSchema = new mongoose.Schema({  // defines the structure
         required: [true, 'Please enter a password'],
         minlength: [6, 'Minimum password length s 6 characters']
     }
+})
+
+// fire a function after doc saved to db
+userSchema.post('save', function (doc, next) { // post here is not related to POST request, the function operates after 'save'
+    console.log('new user was created & saved', doc)
+    next()
+}) 
+
+// fire a function before doc saved to db
+userSchema.pre('save', function (next) {
+    print('user about to be created & saved', this)
+    next()
 })
 
 const User = mongoose.model('user', userSchema)
