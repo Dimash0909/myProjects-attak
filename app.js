26dd5499d280da0495a4fe2906a56161832143d4
Dinmukhammed <tastanbekdimash09@gmail.com>
2024-11-08 23:36:45 +0500

    installed cookies and jsonwebtoken packages

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser')
+const print = require('./utils/utilFuncs')
 require('dotenv').config()
 
 const app = express();
@@ -8,6 +10,7 @@ const app = express();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs');
@@ -25,5 +28,20 @@ mongoose.connect(process.env.dbURI)
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+  //res.setHeader('Set-Cookie', 'newUser=true')
+  res.cookie('newUser', false)
+
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true}) // secure: true - secure means when website is secured "https"  
+  res.send('you got the cookies!')
+})
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies
+  print(cookies)
+  res.json(cookies)
+})
 
 // write to the terminal: npm run server
