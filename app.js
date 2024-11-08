07d05f6bac5dc8556915b40d1c919b52e3bd687a
Dinmukhammed <tastanbekdimash09@gmail.com>
2024-11-09 03:05:48 +0500

    checking and greeting the user in home page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes')
 const cookieParser = require('cookie-parser')
 const print = require('./utils/utilFuncs');
-const { requireAuth } = require('./middleware/authMiddleware');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 require('dotenv').config()
 
 const app = express();
@@ -25,6 +25,7 @@ mongoose.connect(process.env.dbURI)
   .catch((err) => print(err));
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes)
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken')
 const print = require('../utils/utilFuncs')
+const User = require('../models/User')
+
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt
@@ -20,4 +22,27 @@ const requireAuth = (req, res, next) => {
     }
 }
 
-module.exports = { requireAuth }
+// check the current user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt
+
+    if(token) {
+        jwt.verify(token, process.env.JWTsecretPrivateKey, async (err, decodedToken) => {
+            if(err) {
+                print(err.message)
+                res.locals.user = null
+                next()
+            } else {
+                print(decodedToken)
+                let user = await User.findById(decodedToken.id)
+                res.locals.user = user
+                next()
+            }    
+        })
+    } else {
+        res.locals.user = null
+        next()
+    } 
+}
+
+module.exports = { requireAuth, checkUser }
